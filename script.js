cb5429193f9d6cc415d83024df747480a82281ea
dpisati <dpisati@gmail.com>
2021-09-30 09:49:21 +1300

    almost done to test

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function GetTableFromExcel(data) {
 
     //Read all rows from First Sheet into an JSON array.
     var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
-    
+        
     //Create a HTML Table element.
     var myTable  = document.createElement("table");
     myTable.border = "1";
@@ -66,20 +66,13 @@ function GetTableFromExcel(data) {
     var p = excelRows[0];
     for (var key in p) {
         if (p.hasOwnProperty(key)) {
-            var cellValue =  p[key].trim().replace(/\s/g, '_')
-            var columnNumber = key.trim().replace('__EMPTY', '').replace('_', '');
-            
-            if(columnNumber === "") {
-                columnNumber = 0;
-            }
-
+            var columnNumber = key.trim();
             var headerCell = document.createElement("TH");
-            headerCell.innerHTML = cellValue + " " + columnNumber;
-            headers.push(cellValue);
+            headerCell.innerHTML = columnNumber;
+            headers.push(columnNumber);
             row.appendChild(headerCell);
         }
     }
-
     var codeLine = document.createElement("p");
     codeLine.innerHTML += `
     &lt;?xml version="1.0" encoding="UTF-8"?&gt;<br />
@@ -94,110 +87,91 @@ function GetTableFromExcel(data) {
     `;
     codeTag.appendChild(codeLine);
 
-
     //Add the data rows from Excel file.
     for (var i = 1; i < excelRows.length; i++) {
 
         //Add the data row.
         var rowNumber = excelRows[i];
-        var isTheLineEmpty = (rowNumber.__EMPTY_18 === "" ||  rowNumber.__EMPTY === "");
 
-        if(!isTheLineEmpty) {
+        var codeLine = document.createElement("p");
+        codeLine.setAttribute("id", `productName${i}`);
+        codeTag.appendChild(codeLine);
+        var productElement = document.getElementById(`productName${i}`);
 
-            var codeLine = document.createElement("p");
-            codeLine.setAttribute("id", `productName${i}`);
-            codeTag.appendChild(codeLine);
-            var productElement = document.getElementById(`productName${i}`);
+        var row = myTable.insertRow(-1);
 
+        for (var key in excelRows[i]) {
+            if (rowNumber.hasOwnProperty(key)) {
+                var cellValue =  rowNumber[key].trim().replace(/\s/g, ' ');
+                var cellTitle = headers[columnNumber];
+                var codeLine = document.createElement("p");
 
-            var row = myTable.insertRow(-1);
-
-            for (var key in excelRows[i]) {
-                if (rowNumber.hasOwnProperty(key)) {
-                    var cellValue =  rowNumber[key].trim().replace(/\s/g, ' ');
-                    var columnNumber = key.trim().replace('__EMPTY', '').replace('_', '');
-                    if(columnNumber === "") {
-                        columnNumber = 0;
-                    }
-                    var cellTitle = headers[columnNumber];
-
-                    var codeLine = document.createElement("p");
-
-                    if(columnNumber <= 6) {
-
-                        switch(columnNumber) {
-                            case 0:
-                                codeLine.innerHTML += `
-                                    &lt;Quantity&gt;${cellValue}&lt;/Quantity&gt;<br />
-                                `
-                            break;
-                            case "1":
-                                productElement.innerHTML += `
-                                    &lt;Product Name="${cellValue}"&gt;<br />
-                                `;
-                            break;
-                            case "2":
-                                codeLine.innerHTML += `
-                                    &lt;Width&gt;${cellValue}&lt;/Width&gt;<br />
-                                `
-                            break;
-                            case "3":
-                                codeLine.innerHTML += `
-                                    &lt;Height&gt;${cellValue}&lt;/Height&gt;<br />
-                                `
-                            break;
-                            case "4":
-                                codeLine.innerHTML += `
-                                    &lt;Depth&gt;${cellValue}&lt;/Depth&gt;<br />
-                                `
-                            break;
-                            case "5":
-                                codeLine.innerHTML += `
-                                    &lt;LinkIDSpecificationGroup&gt;${cellValue}&lt;/LinkIDSpecificationGroup&gt;<br />
-                                `
-                            break;
-                            case "6":
-                                codeLine.innerHTML += `
-                                    &lt;Comment&gt;${cellValue}&lt;/Comment&gt;<br />
-                                    &lt;Prompts&gt;
-                                `
-                            break;
-                        }
-
-                        codeTag.appendChild(codeLine);
-
-                        if(cellValue && cellValue !== "") {
-                            if(cellTitle) {
-                                var contentCell = document.createElement("TH");
-                                contentCell.innerHTML = cellValue + ' ' + columnNumber;
-                                row.appendChild(contentCell);
-                            }
-                        }
+                if(headers.indexOf(key) <= 6) {
+                    switch(key) {
+                        case "Qty":
+                            codeLine.innerHTML += `
+                                &lt;Quantity&gt;${cellValue}&lt;/Quantity&gt;<br />
+                            `
+                        break;
+                        case "Name":
+                            productElement.innerHTML += `
+                                &lt;Product Name="${cellValue}"&gt;<br />
+                            `;
+                        break;
+                        case "Width":
+                            codeLine.innerHTML += `
+                                &lt;Width&gt;${cellValue}&lt;/Width&gt;<br />
+                            `
+                        break;
+                        case "Height":
+                            codeLine.innerHTML += `
+                                &lt;Height&gt;${cellValue}&lt;/Height&gt;<br />
+                            `
+                        break;
+                        case "Depth":
+                            codeLine.innerHTML += `
+                                &lt;Depth&gt;${cellValue}&lt;/Depth&gt;<br />
+                            `
+                        break;
+                        case "ProductSpecGroupName":
+                            codeLine.innerHTML += `
+                                &lt;LinkIDSpecificationGroup&gt;${cellValue}&lt;/LinkIDSpecificationGroup&gt;<br />
+                            `
+                        break;
+                        case "Comments":
+                            codeLine.innerHTML += `
+                                &lt;Comment&gt;${cellValue}&lt;/Comment&gt;<br />
+                                &lt;Prompts&gt;
+                            `
+                        break;
                     }
 
-                    if(columnNumber > 6) {
-                        if(cellValue && cellValue !== "") {
-                            if(cellTitle) {
-                                var contentCell = document.createElement("TH");
-                                contentCell.innerHTML = cellValue + ' ' + columnNumber;
-                                row.appendChild(contentCell);
-                                codeTag.innerHTML += `
-                                        &lt;Prompt Name="${cellTitle}"&gt;<br />
-                                            &lt;Value&gt;${cellValue}&lt;/Value&gt;<br />
-                                        &lt;/Prompt&gt;<br />
-                                    `
-                            }
-                        }
-                    }
+                    codeTag.appendChild(codeLine);
+
+                    var contentCell = document.createElement("TH");
+                    contentCell.innerHTML = cellValue;
+                    row.appendChild(contentCell);
+                }
+
+                if(headers.indexOf(key) > 6) {
+                    var contentCell = document.createElement("TH");
+                    contentCell.innerHTML = cellValue + "  " + key;
+                    row.appendChild(contentCell);
+                    codeTag.innerHTML += `
+                            &lt;Prompt Name="${key}"&gt;<br />
+                                &lt;Value&gt;${cellValue}&lt;/Value&gt;<br />
+                            &lt;/Prompt&gt;<br />
+                        `
                 }
             }
-
-            codeTag.innerHTML += `
-                    &lt;/Prompts&gt;<br />
-                &lt;/Product&gt;<br />
-                <br /><br /><br /><br />
-            `
         }
+
+        codeTag.innerHTML += `
+                &lt;/Prompts&gt;<br />
+            &lt;/Product&gt;<br />
+            <br /><br /><br /><br />
+        `
+        
     }
     
     codeTag.innerHTML += `
